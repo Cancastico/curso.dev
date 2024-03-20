59cb5eba4f4d51fa26d69a5b60b02c16f63e1655
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-03-20 10:35:18 -0300

    Lint aply

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-function Home(){
-    return (
-        <>
-            <SpeedInsights/>
-            <h1>Um dia conquistaremos tudo aquilo que sonhamos meu amor </h1>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <SpeedInsights />
+      <h1>Um dia conquistaremos tudo aquilo que sonhamos meu amor </h1>
+    </>
+  );
 }
 
 export default Home;
